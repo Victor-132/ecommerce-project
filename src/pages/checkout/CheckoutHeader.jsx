d41c7bf706c814b8af0b68764a65b3842c9bde45
Victor Batata <victorbatata@itstransdata.com>
2025-11-04 14:14:16 -0300

    Separate into smaller components

--- src/pages/checkout/CheckoutHeader.jsx
+++ src/pages/checkout/CheckoutHeader.jsx
@@ -4,7 +4,12 @@ import Logo from '../../assets/images/logo.png'
 import MobileLogo from '../../assets/images/mobile-logo.png'
 import CheckoutLockIcon from '../../assets/images/icons/checkout-lock-icon.png'
 
-export function CheckoutHeader() {
+export function CheckoutHeader({ cart }) {
+  let totalItems = 0
+  cart.forEach(product => {
+    totalItems += product.quantity
+  });
+
   return (
     <div className="checkout-header">
       <div className="header-content">
@@ -17,7 +22,7 @@ export function CheckoutHeader() {
 
         <div className="checkout-header-middle-section">
           Checkout (<Link className="return-to-home-link"
-            to="/">3 items</Link>)
+            to="/">{totalItems} items</Link>)
         </div>
 
         <div className="checkout-header-right-section">
