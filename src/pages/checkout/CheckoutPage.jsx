8d2ec50ddfd7f420ebcd6f5d7d603c0544e59be0
Victor Batata <victorbatata@itstransdata.com>
2025-11-11 14:22:38 -0300

    Separate delivery options and payment summary in two use effects

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -10,17 +10,23 @@ export function CheckoutPage({ cart, loadCart }) {
   const [paymentSummary, setPaymentSummary] = useState(null);
 
   useEffect(() => {
-    const fetchCheckoutData = async () => {
-      let response = await axios.get(
+    const fetchDeliveryOptions = async () => {
+      const response = await axios.get(
         "/api/delivery-options?expand=estimatedDeliveryTime",
       );
       setDeliveryOptions(response.data);
+    };
 
-      response = await axios.get("/api/payment-summary");
+    fetchDeliveryOptions();
+  }, []);
+
+  useEffect(() => {
+    const fetchPaymentSummary = async () => {
+      let response = await axios.get("/api/payment-summary");
       setPaymentSummary(response.data);
     };
 
-    fetchCheckoutData();
+    fetchPaymentSummary();
   }, [cart]);
 
   return (
